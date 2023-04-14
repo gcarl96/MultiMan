b0dc3f784fbaef55ebbfd580f400f65acbfaff84
Gareth Carless <gc15927@my.bristol.ac.uk>
2023-04-14 18:39:30 +0100

    Initial Commit

---
+++ MultiMan/MultiManApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiManApp.swift
+//  MultiMan
+//
+//  Created by Gareth Carless on 14/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MultiManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
